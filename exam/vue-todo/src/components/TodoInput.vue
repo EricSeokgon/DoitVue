5ec633763de040d48374ac16ade2f0918632db6f
hadesyi <leesk55@gmail.com>
2019-01-20 01:07:37 +0900

    텍스트를 저장하기 위한 버튼 이벤트 추가

--- exam/vue-todo/src/components/TodoInput.vue
+++ exam/vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <input type="text" v-model="newTodoItem">
-        <button>추가</button>
+        <button v-on:click="addTodo">추가</button>
     </div>
 </template>
 <script>
@@ -10,7 +10,10 @@ export default {
         return{
             newTodoItem:''
         }
-    }
+    },
+    methods: {
+        console.log(this.newTodoItem);
+    },
 }
 </script>
 <style>
